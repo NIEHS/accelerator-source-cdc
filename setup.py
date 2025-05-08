0ad47018856f4ff9da6c540e6a12627fe31c0586
conwaymc <mike.conway@nih.gov>
2025-05-08 09:16:09 -0400

    #2 refactor location requirements and setup for packaging

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt", "r") as f:
+    install_requires = [line.strip() for line in f if line.strip()]
+
 setup(
     name="accelerator-source-cdc",
     version="0.1.0",
-    description="Ingest and crosswalk for CDC data",
-    author="Sue Nolte",
-    author_email="sue.nolte@nih.gov",
-    url="https://github.com/NIEHS/accelerator-source-cdc",
+    description="accelerator source cdc ingest",
+    author="Mike Conway",
+    author_email="mike.conway@nih.gov",
+    url="https://github.com/szcc/accelerator-source-cdc",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=install_requires,#[open("requirements.txt").read()],
     license="BSD 3-Clause",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.9"
+    python_requires=">=3.9",
 )
